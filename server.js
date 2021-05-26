6e11258e2a13a6e39d76f0e49dc5a004ef21dab6
Amanda Hoshor <hoshor.manda@gmail.com>
2021-05-25 20:08:41 -0600

    added mySQl paths, tested the connection

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 //imports express
 const express = require('express');
+const mysql = require('mysql2');
 
 //adding the PORT and app expression
 const PORT = process.env.PORT || 3012;
 const app = express();
 
-//adding the middleware
+//adding the Express middleware
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
-
+//connection to the database(mysql)
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //MySQL username
+        user: 'root',
+        //MySQL passwoor
+        password: 'Loralai!76',
+        database:'election'
+    },
+    console.log('Connected to the election database.')
+);
+//querying the database to test connection
+db.query( `SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 //GET route to handle user requests, Default response for any other request (Not Found)
 app.use((req, res) => {
